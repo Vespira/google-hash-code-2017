d18c975296d92c11367e74755473a955028bcdc2
Mikaël FRECHE <mikael.freche@pharmagest.com>
2017-02-23 19:28:55 +0100

    First shot

---
+++ src/main/java/com/company/Cache.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * Created by mfreche on 23/02/17.
+ */
+@AllArgsConstructor
+public class Cache {
+
+    int id;
+    int size;
+
+}
