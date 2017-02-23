433c80a8f670c2b5d8f1f748bea92f9d8ae58a32
Mikaël FRECHE <mikael.freche@pharmagest.com>
2017-02-23 19:29:53 +0100

    First shot

---
+++ src/main/java/com/company/Endpoint.java
@@ -0,0 +1,20 @@
+package com.company;
+
+import lombok.AllArgsConstructor;
+
+import java.util.Map;
+
+/**
+ * Created by mfreche on 23/02/17.
+ */
+@AllArgsConstructor
+public class Endpoint {
+
+    /**
+     * Id cache -> latence
+     */
+    Map<Cache, Integer> cacheConnexions;
+
+    Integer latencyToDataCenter;
+
+}
---
+++ src/main/java/com/company/Video.java
@@ -0,0 +1,13 @@
+package com.company;
+
+import lombok.AllArgsConstructor;
+
+/**
+ * Created by mfreche on 23/02/17.
+ */
+@AllArgsConstructor
+public class Video {
+
+    int size;
+
+}
