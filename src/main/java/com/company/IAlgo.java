86b0150cd9fccef6565d9e9d7d704cffc0693cab
Mikaël FRECHE <mikael.freche@pharmagest.com>
2017-02-23 19:56:25 +0100

    First shot

---
+++ src/main/java/com/company/IAlgo.java
@@ -0,0 +1,12 @@
+package com.company;
+
+import java.util.Map;
+
+/**
+ * Created by mfreche on 23/02/17.
+ */
+public interface IAlgo {
+
+    Map<Cache, Video> trouveMeilleurSolution(Situation situation);
+
+}
